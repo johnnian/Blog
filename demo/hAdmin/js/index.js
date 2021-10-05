3e0e9e5be8b91b4a4aeb37bf8e045e686c329ba4
Johnnian <jesusloveyou_1991@126.com>
2021-10-05 17:35:27 +0800

    删除hAdmin的DEMODEMO

--- demo/hAdmin/js/index.js
+++
@@ -1,9 +0,0 @@
-$(function(){
-    //菜单点击
-    J_iframe
-    $(".J_menuItem").on('click',function(){
-        var url = $(this).attr('href');
-        $("#J_iframe").attr('src',url);
-        return false;
-    });
-});
